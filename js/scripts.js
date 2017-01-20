09b6c4d7f2b78f9e15293c49af20546a9d7acbaf
saidahmed <ahmeds5@uw.edu>
2017-01-20 11:56:22 -0800

    add the specification of having the function return ping if a number is divisible by 3.

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,8 @@ var pingPong = function(userNumber){
         result.push('ping-pong');
     } else if(i % 5 === 0) {
         result.push('pong');
+    } else if(i % 3 === 0) {
+        result.push('ping');
     } else {
         result.push(i);
     }
